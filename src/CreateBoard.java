debc0ed883e720e74f19c5e4e6236399fc549a88
ege <e.ge.ozcan1907@hotmail.com>
2018-12-21 22:24:36 +0300

    create boarda block ekledim

--- src/CreateBoard.java
+++ src/CreateBoard.java
@@ -1,3 +1,5 @@
+import javafx.beans.property.ObjectProperty;
+import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.embed.swing.SwingFXUtils;
 import javafx.event.ActionEvent;
@@ -8,10 +10,7 @@ import javafx.geometry.Pos;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.Alert;
-import javafx.scene.control.Button;
-import javafx.scene.control.DialogPane;
-import javafx.scene.control.TextField;
+import javafx.scene.control.*;
 import javafx.scene.image.WritableImage;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.*;
@@ -24,6 +23,10 @@ import java.io.File;
 import java.io.FileWriter;
 import java.io.PrintWriter;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+import java.util.Random;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -31,11 +34,27 @@ public class CreateBoard {
     public enum Color {
         BLACK, WHITE, DEEPPINK, GRAY, SPRINGGREEN, RED, YELLOW, DEEPSKYBLUE, PURPLE, ORANGE
     }
-    public Button back,test;
+
+    public Button back, test;
+    public GridPane block0Grid, block1Grid, block2Grid, block3Grid, block4Grid, block5Grid, block6Grid, block7Grid, block8Grid, block9Grid;
     private String userBoardName;
     private String dir;
     private String choosenColor = "DEEPPINK"; // inital color is pink
-    public void backClicked()throws Exception{
+
+    private  String [] hints ={"Get Better","E = mc^2", "Use Shift and hover over cells to paint", "zego adam"};
+    //private ChoiceBox<Integer> blockNum1 = new ChoiceBox<>();//=  new ChoiceBox(FXCollections.observableArrayList(hints));
+    public void hintClicked() throws Exception {
+        Random rand  = new Random();
+        int randomNum = rand.nextInt((hints.length));
+        String hint = hints[randomNum];
+        Alert alert = new Alert(Alert.AlertType.INFORMATION);
+        alert.setTitle("Hint");
+        alert.setHeaderText(null);
+        alert.setContentText(hint);
+        alert.showAndWait();
+    }
+
+    public void backClicked() throws Exception {
         Parent loader = FXMLLoader.load(getClass().getResource("fxml/sample.fxml"));//Creates a Parent called loader and assign it as leaderboard.FXML
         Scene scene = new Scene(loader); //This creates a new scene called scene and assigns it as the Sample.FXML document which was named "loader"
         Stage app_stage = (Stage)back.getScene().getWindow();
@@ -44,18 +63,48 @@ public class CreateBoard {
         app_stage.show(); // this shows the scene
     }
 
-    public void blackClicked()throws Exception{ choosenColor = "BLACK"; }
-    public void whiteClicked()throws Exception{ choosenColor = "WHITE"; }
-    public void pinkClicked()throws Exception{ choosenColor = "DEEPPINK"; }
-    public void grayClicked()throws Exception{ choosenColor = "GRAY"; }
-    public void greenClicked()throws Exception{ choosenColor = "SPRINGGREEN"; }
-    public void redClicked()throws Exception{ choosenColor = "RED"; }
-    public void yellowClicked()throws Exception{ choosenColor = "YELLOW"; }
-    public void blueClicked()throws Exception{ choosenColor = "DEEPSKYBLUE"; }
-    public void purpleClicked()throws Exception{ choosenColor = "PURPLE"; }
-    public void orangeClicked()throws Exception{ choosenColor = "ORANGE"; }
+    public void blackClicked() throws Exception {
+        choosenColor = "BLACK";
+    }
 
-    public void testClicked()throws Exception{ }
+    public void whiteClicked() throws Exception {
+        choosenColor = "WHITE";
+    }
+
+    public void pinkClicked() throws Exception {
+        choosenColor = "DEEPPINK";
+    }
+
+    public void grayClicked() throws Exception {
+        choosenColor = "GRAY";
+    }
+
+    public void greenClicked() throws Exception {
+        choosenColor = "SPRINGGREEN";
+    }
+
+    public void redClicked() throws Exception {
+        choosenColor = "RED";
+    }
+
+    public void yellowClicked() throws Exception {
+        choosenColor = "YELLOW";
+    }
+
+    public void blueClicked() throws Exception {
+        choosenColor = "DEEPSKYBLUE";
+    }
+
+    public void purpleClicked() throws Exception {
+        choosenColor = "PURPLE";
+    }
+
+    public void orangeClicked() throws Exception {
+        choosenColor = "ORANGE";
+    }
+
+    public void testClicked() throws Exception {
+    }
 
     public void clearClicked()throws Exception{
          clear();
@@ -63,7 +112,9 @@ public class CreateBoard {
 
 
     @FXML
-    private GridPane grid ;
+    private GridPane grid;
+    @FXML
+    private ChoiceBox<Integer> blockNum0,blockNum1,blockNum2,blockNum3,blockNum4,blockNum5,blockNum6,blockNum7,blockNum8,blockNum9;
 
 
     private void takeSnapShot(Scene scene, String board) {
@@ -81,6 +132,18 @@ public class CreateBoard {
     }
 
     public void initialize() {
+        blockNum0.getItems().addAll(0,1,2,3,4,5,6,7,8,9); blockNum0.setValue(0);
+        blockNum1.getItems().addAll(0,1,2,3,4,5,6,7,8,9); blockNum1.setValue(0);
+        blockNum2.getItems().addAll(0,1,2,3,4,5,6,7,8,9); blockNum2.setValue(0);
+        blockNum3.getItems().addAll(0,1,2,3,4,5,6,7,8,9); blockNum3.setValue(0);
+        blockNum4.getItems().addAll(0,1,2,3,4,5,6,7,8,9); blockNum4.setValue(0);
+        blockNum5.getItems().addAll(0,1,2,3,4,5,6,7,8,9); blockNum5.setValue(0);
+        blockNum6.getItems().addAll(0,1,2,3,4,5,6,7,8,9); blockNum6.setValue(0);
+        blockNum7.getItems().addAll(0,1,2,3,4,5,6,7,8,9); blockNum7.setValue(0);
+        blockNum8.getItems().addAll(0,1,2,3,4,5,6,7,8,9); blockNum8.setValue(0);
+        blockNum9.getItems().addAll(0,1,2,3,4,5,6,7,8,9); blockNum9.setValue(0);
+
+
 
         //Save board button and its function
         test.setOnAction(event -> {TextField text = new TextField("Enter Level Name");
@@ -158,8 +221,9 @@ public class CreateBoard {
         });
 
         //Adding white panes to every grid
-        int numCols = 20 ;
-        int numRows = 20 ;
+        int numCols = 20;
+        int numRows = 20;
+        createBlocks();
 
         for (int i = 0 ; i < numCols ; i++) {
             ColumnConstraints colConstraints = new ColumnConstraints();
@@ -251,14 +315,11 @@ public class CreateBoard {
                 else
                     writer.println("01");
             }*/
-            }
-
-           Node node;
-//asd
-            for (int i =0;i<20;i++)
-            {
-                for (int j =0;j<20;j++) {
-                    node  = childrens.get(i+(20*j)+1);
+            }Node node;
+            //writing cell info
+            for (int i = 0; i < 20; i++) {
+                for (int j = 0; j < 20; j++) {
+                    node = childrens.get(i + (20 * j) + 1);
                     //System.out.print("j is "+j+", i is " +i+ " ");
                     style = node.getStyle();
                     //System.out.println(style);
@@ -278,11 +339,56 @@ public class CreateBoard {
                 }
             }
             writer.close();
-        }catch (IOException e){
-        //do something 
+
+           //writing block info
+            blockwriter.println(blockNum0.getValue());
+            blockwriter.println(blockNum1.getValue());
+            blockwriter.println(blockNum2.getValue());
+            blockwriter.println(blockNum3.getValue());
+            blockwriter.println(blockNum4.getValue());
+            blockwriter.println(blockNum5.getValue());
+            blockwriter.println(blockNum6.getValue());
+            blockwriter.println(blockNum7.getValue());
+            blockwriter.println(blockNum8.getValue());
+            blockwriter.println(blockNum9.getValue());
+
+            blockwriter.close();
+        } catch (IOException e) {
+            //do something
         }
     }
 
 
+    private void createBlocks() {
+        //Get gui grids
+        GridPane[] guiBlockGrids = {block0Grid, block1Grid, block2Grid, block3Grid, block4Grid, block5Grid, block6Grid, block7Grid, block8Grid, block9Grid};
+        //Label[] guiBlockAmounts = {block0Amount , block1Amount , block2Amount , block3Amount , block4Amount , block5Amount , block6Amount , block7Amount , block8Amount , block9Amount, moveCountLabel};
+        //Get blocks
+        int[] blockNumbers = {1, 1, 1, 1, 1, 1, 1, 1, 1,1};//myBoard.getBoardBlocks();
 
+        for (int blockIndex = 0; blockIndex < 10; blockIndex++) {
+            if (blockNumbers[blockIndex] != 0) {
+                Block myBlock = new Block(Block.BlockShape.values()[blockIndex]);
+                Cell[][] blockGrid = myBlock.getBlockShape();
+                List<ObjectProperty<EventHandler<? super MouseEvent>>> myList = new ArrayList<>();
+
+//                guiBlockGrids[blockIndex].setGridLinesVisible(true);
+                for (int i = 0; i < 3; i++) {
+                    for (int j = 0; j < 3; j++) {
+                        if (blockGrid[i][j].getVisible()) {
+                            System.out.print(blockGrid[i][j].getVisible() + ",");
+                            Pane canvas = new Pane();
+                            canvas.setStyle("-fx-background-color: RED;");
+                            canvas.setBorder(new Border(new BorderStroke(javafx.scene.paint.Color.BLACK, BorderStrokeStyle.SOLID, CornerRadii.EMPTY, BorderWidths.DEFAULT)));
+                            //guiBlockAmounts[blockIndex].setText("x" + blockNumbers[blockIndex]);
+                            guiBlockGrids[blockIndex].setConstraints(canvas, i, j);
+                            guiBlockGrids[blockIndex].getChildren().addAll(canvas);
+                        }
+                    }
+                }
+            }
+        }
+
+
+    }
 }
